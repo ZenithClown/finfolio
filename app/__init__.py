074c8c29faae77f21f26299fe57016ef5fca649a
Debmalya Pramanik <DebmalyaPramanik.005@gmail.com>
2021-12-07 09:17:58 +0530

    add package headers

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
 # -*- encoding: utf-8 -*-
+
+__author__       = "Debmalya Pramanik"
+__author_email__ = "pOrgz@tuta.io"
+
+__credits__      = None
+
+__status__       = "Development"
+__version__      = open("VERSION", "r").read()
+__docformat__    = "camelCasing"
+
+__copyright__    = "Copyright (c) 2020 Debmalya Pramanik"
+__affiliation__  = "Reliance Jio Platforms Ltd. | IIT Dhanbad"
