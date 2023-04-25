d69286a8c4ecf7d655dc5fce193a00843fd72b35
ZenithClown <debmalyapramanik.005@gmail.com>
2023-04-25 13:29:24 +0530

    ✨ create signup interface and api

--- app/main/application/api/signup.py
+++ app/main/application/api/signup.py
@@ -9,6 +9,10 @@ the database. The API is thus defined to create a new user.
 
 from app.main.application._base_resource import BaseResource
 from app.main.repository.interface.users import UsersTableInterface
+from app.main.repository.interface.authentication import (
+    LastPasswordInterface,
+    UserAuthenticationInterface
+)
 
 class SignUp(BaseResource):
     """
@@ -44,8 +48,15 @@ class SignUp(BaseResource):
 
         # database access repository/interfaces modules
         self.users_tbl_interface = UsersTableInterface()
+        self.last_pass_tbl_interface = LastPasswordInterface()
+        self.users_auth_tbl_interface = UserAuthenticationInterface()
 
     
     def post(self):
+        # ! WRONG IMPLEMENTATION - same variable is overwritten
+        # TODO Fix the same variable name, and possibly create a loop to parse data
         err, msg_desc, success = self.users_tbl_interface.post_user(**self.args)
+        err, msg_desc, success = self.last_pass_tbl_interface.post_user(username = self.args["username"])
+        err, msg_desc, success = self.users_auth_tbl_interface.post_user(username = self.args["username"])
+        
         return self.formatter.post(code = 200 if success else 404, err = err, msg_desc = msg_desc)
