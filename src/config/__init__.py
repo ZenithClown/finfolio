f89f57d5d69c591f9d5d271b196b22d92b1fa23f
ZenithClown <debmalyapramanik.005@gmail.com>
2024-01-04 23:16:00 +0530

    💣 remove logging, refactor code imports, set config as module

---
+++ src/config/__init__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Global Configurations, Variables and Module Setup
+
+The module is configured for both CLI/APP purpose using some global
+configurations or variables, which are directly imported.
+"""
+
+import os
+
+from config.skeleton import setupHome, getOrFetchDB
+
+APP_HOME = setupHome() # ! home directory, should be parameterized
+APP_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+
+# all application data is stored in serverless, sqlite3 database
+# todo: enable encryption logic on db file with hashing algorithm
+APP_DATA, _ = getOrFetchDB(dbFile = os.path.join(APP_HOME, "pOrgz.db"))
+
+# using parameterized raw-sql for db communication
+DB_QUERIES = os.path.join(APP_ROOT, "db")
+
+DB_VIEWS = os.path.join(DB_QUERIES, "views")
+DB_MODELS = os.path.join(DB_QUERIES, "models")
