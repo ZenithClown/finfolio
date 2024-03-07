a65b97c7b336daaa3eb247be1ae25bab66771906
ZenithClown <debmalyapramanik.005@gmail.com>
2024-03-07 21:41:37 +0530

    ✨ initialize server class for finfolio

---
+++ server/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+"""
+The Server Class (i.e. the backend) for the FinFolio-DB Application
+
+The server module presents one-stop solutions for backend jobs, like
+write/update/delete operation on the tables. Check individual section
+for more information on usages.
+
+@author:  nxLogics, Debmalya Pramanik
+@version: v0.0.1
+"""
