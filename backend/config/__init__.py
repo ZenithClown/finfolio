0bec63e462b3b1bc40d64d0ea8a164f2cb0131a9
ZenithClown <debmalyapramanik.005@gmail.com>
2024-03-11 23:04:23 +0530

    🛠✨ add config from pOrgz to finfolio/backend

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -9,18 +9,20 @@ configurations or variables, which are directly imported.
 
 import os
 
-from config.skeleton import setupHome, getOrFetchDB
+# import the skeleton for config
+# ! may delete on configurations
+# ? using relative import in file
+import skeleton as skl
 
-APP_HOME = setupHome() # ! home directory, should be parameterized
-APP_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+APP_HOME = skl.setupHome() # ! home directory, should be parameterized
+APP_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")
 
 # all application data is stored in serverless, sqlite3 database
 # todo: enable encryption logic on db file with hashing algorithm
-APP_DATA, DB_CURSOR = getOrFetchDB(dbFile = os.path.join(APP_HOME, "pOrgz.db"))
+APP_ENGINE, DB_CURSOR = skl.getOrFetchDB(dbFile = os.path.join(APP_HOME, "pOrgz.db"))
 
 # using parameterized raw-sql for db communication
-DB_STRUCT = os.path.join(APP_ROOT, "db")
+DB_STRUCT = os.path.join(APP_ROOT, "database")
 
 DB_VIEWS = os.path.join(DB_STRUCT, "views")
 DB_MODELS = os.path.join(DB_STRUCT, "models")
-DB_QUERIES = os.path.join(DB_STRUCT, "queries", "statements")
