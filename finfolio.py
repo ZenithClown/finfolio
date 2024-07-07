63e4fa3db8985c2304fc8f6d84281d00af4bea4f
ZenithClown <debmalyapramanik.005@gmail.com>
2024-07-08 00:41:39 +0530

    🛠️ refactor finfolio with create all tables

--- finfolio.py
+++ finfolio.py
@@ -7,8 +7,15 @@ The file serves as the main file to start and serve the project
 to end user, and integrates backend with frontend.
 """
 
+from backend.main import engine
 from backend import USER_HOME_DIRECTORY
+
+from backend.app.api.models import * # noqa: F401, F403 # pyright: ignore[reportMissingImports]
+from backend.app.api.base import model
+
 import backend.app.config.skeleton as skeleton
+
 
 if __name__ == "__main__":
     skeleton.setupHome(USER_HOME_DIRECTORY) # ? initialize project path
+    model.metadata.create_all(engine)
