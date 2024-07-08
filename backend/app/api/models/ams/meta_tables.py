0112998c72ea6eab7e6d685531f1bd518c9fb2e8
ZenithClown <debmalyapramanik.005@gmail.com>
2024-07-08 19:56:21 +0530

    🛠️ create relationship in ams metadata tables

--- backend/app/api/models/ams/meta_tables.py
+++ backend/app/api/models/ams/meta_tables.py
@@ -8,7 +8,9 @@ account, grouping/clubbing transactions and inter-linking of details
 based on the criteria. Check model definations for more information.
 """
 
-from sqlalchemy import Column, VARCHAR
+from sqlalchemy.orm import Relationship
+from sqlalchemy import Column, VARCHAR, ForeignKey
+
 from backend.app.api.base import BaseModel
 
 class META_ACCOUNT_TYPE(BaseModel):
@@ -18,9 +20,16 @@ class META_ACCOUNT_TYPE(BaseModel):
     account_type_name = Column(VARCHAR(16), unique = True, nullable = False) # typically, the display name
     account_type_desc = Column(VARCHAR(64), nullable = False) # description of the account type, user aware
 
+    _fk_p2c_acc_type = Relationship("META_SUBACCOUNT_TYPE", back_populates = "ams.META_ACCOUNT_TYPE", passive_deletes = True)
+
 class META_SUBACCOUNT_TYPE(BaseModel):
     __tablename__ = "ams.META_SUBACCOUNT_TYPE"
 
     account_subtype = Column(VARCHAR(3), primary_key = True, nullable = False, autoincrement = False)
+
+    # ? each account sub type is unique 3 digit code, and is linked to a parent type
+    account_type = Column(VARCHAR(3), ForeignKey("ams.META_ACCOUNT_TYPE.account_type", ondelete = "CASCADE"), nullable = False, index = True)
+    _fk_c2p_acc_type = Relationship("META_ACCOUNT_TYPE", back_populates = "_fk_p2c_acc_type")
+
     account_subtype_name = Column(VARCHAR(16), unique = True, nullable = False) # typically, the display name
     account_subtype_desc = Column(VARCHAR(64), nullable = False) # description of the account type, user aware
