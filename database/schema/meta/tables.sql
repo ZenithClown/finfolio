9c1cbebaebe9ebf08486178bd479fe091c72f9f1
ZenithClown <debmalyapramanik.005@gmail.com>
2024-10-04 23:52:25 +0530

    🛠 fix name constraint in table, add in documentation

--- database/schema/meta/tables.sql
+++ database/schema/meta/tables.sql
@@ -131,12 +131,13 @@ CREATE TABLE IF NOT EXISTS meta.ledger_account_detail (
 
   account_name
     VARCHAR(64)
-    UNIQUE NOT NULL,
+    CONSTRAINT uq_account_name UNIQUE
+    NOT NULL,
 
   account_owner
     VARCHAR(16)
     NOT NULL
-    CONSTRAINT fk_account_owner FOREIGN KEY
+    CONSTRAINT fk_account_owner
       REFERENCES meta.user_account_detail (username)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
@@ -144,14 +145,14 @@ CREATE TABLE IF NOT EXISTS meta.ledger_account_detail (
   account_type_id
     VARCHAR(3)
     NOT NULL
-    CONSTRAINT fk_ledger_account_type_id FOREIGN KEY
+    CONSTRAINT fk_ledger_account_type_id
       REFERENCES meta.account_type_detail  (account_type_id)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
 
   account_subtype_id
     VARCHAR(3)
-    CONSTRAINT fk_ledger_sub_account_type_id FOREIGN KEY
+    CONSTRAINT fk_ledger_sub_account_type_id
       REFERENCES meta.account_subtype_detail  (account_subtype_id)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
@@ -166,13 +167,12 @@ CREATE TABLE IF NOT EXISTS meta.ledger_account_detail (
   account_marked_inactive_on
     DATE,
 
-  openeing_balance
+  opening_balance
     NUMERIC(19, 2)
     NOT NULL
     DEFAULT 0.00,
 
-  openeing_balance_recorded_on
+  opening_balance_recorded_on
     DATE
-    NOT NULL
-    DEFAULT openeing_balance
+    NOT NULL -- ? DEFAULT account_opened_on
 );
