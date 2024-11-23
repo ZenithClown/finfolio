55cc4c2e469fdea31a68c9eda18c4e90c413188e
ZenithClown <debmalyapramanik.005@gmail.com>
2024-11-23 22:10:05 +0530

    📃💣 add docs, refactor function name

--- admin/functions/udf_get_to_update_record_for_transaction_method.sql
+++ admin/functions/udf_get_to_update_record_for_transaction_method.sql
@@ -1,4 +1,16 @@
-CREATE OR REPLACE FUNCTION public.udf_get_to_update_record_for_transaction_method (reference VARCHAR(64))
+/********************************************************************
+Pattern Filter for Transfer Method
+
+A simple function can be used to filter and updated the records for
+transfer methods which can be any one of the following defined under
+the ``meta.transaction_method`` table. This function simply searches
+for the pattern using the ``LIKE`` clause and reports the same to be
+used in the stored procedure ``public.set_transfer_method`` to update
+the records in one go. Check the
+``scripts/exec_updated_transfer_method.sql`` for more information.
+********************************************************************/
+
+CREATE OR REPLACE FUNCTION public.udf_pattern_filter_for_transfer_method (wpattern VARCHAR(64))
   RETURNS TABLE (
     transaction_id BIGINT
     , account_id   CHAR(5)
