ddaaa35c1b31b4f2c85b894e3039a4ced79f999f
ZenithClown <debmalyapramanik.005@gmail.com>
2024-11-23 21:25:22 +0530

    ✨ register two new transfer method - cash and credit card

--- database/seed/transaction_type_method.sql
+++ database/seed/transaction_type_method.sql
@@ -13,4 +13,6 @@ INSERT INTO meta.transaction_method (method_name, method_desc) VALUES
   ('IMPS', 'Immediate Payment Service'),
   ('RTGS', 'Real Time Gross Settlement'),
   ('UPI', 'United Payment Interface'),
-  ('MANDATE', 'Automatic Standing Instructions');
+  ('MANDATE', 'Automatic Standing Instructions'),
+  ('CASH', 'Cash Withdrawl/Deposit'),
+	('CREDIT', 'Transactions via a Credit Card');
