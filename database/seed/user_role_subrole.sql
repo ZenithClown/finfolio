e7ab036f1fcc87430ddb76bcc720149a3dad6fd4
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-21 21:39:38 +0530

    💣 refactor user roles int/ext+
    - internal user roles are removed from the seed data; maybe used in future development
    - external users are completely removed from the seed data; this type of annotations makes it difficult to maintain

--- database/seed/user_role_subrole.sql
+++ database/seed/user_role_subrole.sql
@@ -1,19 +1,18 @@
 /********************************************************************
-User Role & Sub-Role Initial Seed Value for FINFOLIO
+User Role & Sub-Role Initial Seed Value for Project FINFOLIO
 
-Copywright © [2024] Debmalya Pramanik
+The user role and sub-roles are defined for RLS and CLS purposes, and
+is left for an end user to maintain (or maybe will be integrated at a
+future point of application development). The roles gives more control
+like implementation of security, access control and viewing rights.
+
+Curretntly, the user subroles are kept for future developments only,
+and not used in the project.
+
+Copywright © [2024] Debmalya Pramanik (ZenithClown)
 ********************************************************************/
 
 INSERT INTO meta.user_role (role_id, role_name, role_desc) VALUES
   (1, 'ROOT', 'Root/Super Administrator User'),
   (2, 'SUDO', 'A User with Elevated Privileges'),
-  (3, 'USER', 'Normal User with Viewing Rights'),
-  (4, 'INT+', 'Internal Normal User without Viewing Rights'),
-  (5, 'EXT+', 'External User Typically having a Transactional Relationships');
-
-INSERT INTO meta.user_subrole (subrole_id, role_id, subrole_name, subrole_desc) VALUES
-  (1, 5, 'FAMILY', 'The user is part of the family, and his account may be tracked.'),
-  (2, 5, 'RELATIVE', 'The user is part of the extended family and typically does not have a tracked account.'),
-  (3, 5, 'FRIENDS', 'A friend of the user, who typically has a transactional relationship.'),
-  (4, 5, 'ORGANIZATION', 'An organization which has a transactional relationship, example companies where the user has worked.'),
-  (5, 5, 'EXTERNAL', 'An external user who is not a friend, relative, family or an organization.');
+  (3, 'USER', 'Normal User with Viewing Rights');
