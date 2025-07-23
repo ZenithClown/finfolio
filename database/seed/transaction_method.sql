4016de8c61e11015913280b80c612b28ebeeb0a1
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-24 00:07:58 +0530

    🩹 add deleted content from cf3743e09ca14a4ac26ed2df48741dec76fadb7a; transaction methods

---
+++ database/seed/transaction_method.sql
@@ -0,0 +1,9 @@
+INSERT INTO meta.transaction_method (method_name, method_desc) VALUES
+  ('NEFT', 'National Electronic Funds Transfer'),
+  ('IMPS', 'Immediate Payment Service'),
+  ('RTGS', 'Real Time Gross Settlement'),
+  ('UPI', 'United Payment Interface'),
+  ('MANDATE', 'Automatic Standing Instructions'),
+  ('CASH', 'Cash Withdrawl/Deposit'),
+	('CREDIT', 'Transactions via a Credit Card'),
+  ('NACH', 'National Automated Clearing House');
