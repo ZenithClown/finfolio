cf3743e09ca14a4ac26ed2df48741dec76fadb7a
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-24 00:07:36 +0530

    💣 delete transaction method from types

---
+++ database/seed/transaction_type.sql
@@ -0,0 +1,9 @@
+/********************************************************************
+Code Description
+
+Copywright © [2024] Debmalya Pramanik
+********************************************************************/
+
+INSERT INTO meta.transaction_type (type_id, trx_type_detail) VALUES
+  ('D', 'Deposit into the Account'),
+  ('W', 'Withdraw from the Account');
