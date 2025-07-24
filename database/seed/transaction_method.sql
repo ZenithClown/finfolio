655420c440034676e29338542b265d5f7d47f6a2
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-24 22:10:07 +0530

    🩹 all seed data for meta moved under meta/seed.sql

--- database/seed/transaction_method.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO meta.transaction_method (method_name, method_desc) VALUES
-  ('NEFT', 'National Electronic Funds Transfer'),
-  ('IMPS', 'Immediate Payment Service'),
-  ('RTGS', 'Real Time Gross Settlement'),
-  ('UPI', 'United Payment Interface'),
-  ('MANDATE', 'Automatic Standing Instructions'),
-  ('CASH', 'Cash Withdrawl/Deposit'),
-	('CREDIT', 'Transactions via a Credit Card'),
-  ('NACH', 'National Automated Clearing House');
