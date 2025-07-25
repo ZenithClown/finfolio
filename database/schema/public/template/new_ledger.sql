dc4172a732877a3efe783240c648962d6d8769f7
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-25 21:58:34 +0530

    ⭐ create a template code to create a new ledger account for an user - the template uses the PgSQL GEN_RANDOM_UUID() function to automatically create an unique identity for a ledger account - modify the template statement to create a value on its own - 🚧 WIP the template script may be extended to an external application

---
+++ database/schema/public/template/new_ledger.sql
@@ -0,0 +1,23 @@
+/********************************************************************
+Template Code to Create a New Ledger Account for a User
+
+The template statement can be used to create a new ledger account for
+a user. The template statement may also be integrated with an external
+API to create a new ledger account for a user.
+
+Copywright © [2025] Debmalya Pramanik (ZenithClown)
+********************************************************************/
+
+INSERT INTO public.ledger_account_detail (
+  ledger_account_id
+  , account_name
+  , account_owner
+  , account_type_id
+  , account_subtype_id
+  , account_opened_on
+  , account_closed_on
+  , account_marked_inactive_on
+  , opening_balance
+  , opening_balance_recorded_on
+) VALUES
+  (LEFT(UPPER(GEN_RANDOM_UUID()::VARCHAR(36)), 5), ?, ?, ?, ?, ?, ?, ?, ?, ?);
