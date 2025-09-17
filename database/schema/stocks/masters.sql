15bf4c88d67dcb9fd6bb75147a5b1ca1fe03653e
ZenithClown <debmalyapramanik.005@gmail.com>
2025-09-17 23:35:51 +0530

    ✨ create table to track stock symbols as on different exchanges

--- database/schema/stocks/masters.sql
+++ database/schema/stocks/masters.sql
@@ -1,3 +1,16 @@
+/********************************************************************
+Master Table for Stocks Data - Extended Schema for Project FINFOLIO
+
+Stocks data are stored for all available assets in the market. Check
+more details on GH#8 for more informations.
+
+  SHA-256 HEX Digest for Asset Symbol : Combination is generated from
+    ISIN + Stock Exchange ENUM Value; The data is stored in format
+    CHAR(64), which is always a ``str.upper()`` formatted data.
+
+Copyright © [2025] Debmalya Pramanik (ZenithClown)
+********************************************************************/
+
 CREATE TABLE stocks.assets_master (
   isin_code
     CHAR(12) NOT NULL
@@ -8,6 +21,26 @@ CREATE TABLE stocks.assets_master (
 
   asset_class
     stocks.asset_class NOT NULL
+);
+
+
+CREATE TABLE stocks.asset_symbol_master (
+  hexdigest
+    CHAR(64) NOT NULL
+    CONSTRAINT pk_hex_asset_symbol PRIMARY KEY,
+
+  isin_code
+    CHAR(12) NOT NULL
+    CONSTRAINT fk_asset_symbol_isin
+      REFERENCES stocks.assets_master (isin_code)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+
+  stock_exchange
+    stocks.stock_exchange NOT NULL,
+
+  stock_symbol
+    VARCHAR(32) NOT NULL
 );
 
 
