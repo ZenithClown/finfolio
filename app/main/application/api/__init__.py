15a1fba1d851e807757b681da76ceec21cd5888e
E33605 <debmalyapramanik.005@gmail.com>
2025-10-10 16:39:47 +0530

    💣🔮 replace with node application - nextjs
    - check 4a7ae4be43620cd5443f37358bc4b1cfc7bc4f71 for more details

--- app/main/application/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from app.main.application.api.admin import * # noqa: F401, F403
-from app.main.application.api.roles import * # noqa: F401, F403
-from app.main.application.api.signup import * # noqa: F401, F403
--- app/main/controller/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from .oauthControllers import OAuthController
