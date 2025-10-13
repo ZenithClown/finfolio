58c79ebb0bba3e5a0e37e113fb1fe3c23c74c59b
E33605 <debmalyapramanik.005@gmail.com>
2025-10-13 12:06:44 +0530

    💣✨ create query to fetch ledger account from session user - remove redundant query to get all the ledger accounts - instead the function accepts username and account type for dynamic and global searches

---
+++ database/api/security/accounts.sql
@@ -0,0 +1,85 @@
+/********************************************************************
+A Code to Fetch Account Details for a Logged In User for Analysis
+
+A logged in user can view all the accounts that is either directly
+managed by them or can view all the accounts that are managed by user
+under the ROOT/SUDO list while a USER can view only their account.
+
+Documentation for the function with changelog (if any) is documented
+as below:
+
+Get Ledger Account JSON : /rpc/get_ledger_account_json
+======================================================
+
+Get the list of ledger accounts for an already logged in user based
+on the function that returns the value in a JSON format like below:
+
+```json
+[
+  {'ledger_account_id' : 'account_name'},
+  {'ledger_account_id' : 'account_name'},
+  ...
+]
+```
+
+The function is designed to fetch all the debit accounts (by default)
+that are directly mapped under a user or is under a user who is
+managed by the logged in user, with an optional parameter to fetch the
+type of account. To fetch all the account pass `NULL` as the second
+argument, or pass the like clause key for global search.
+
+The function is dynamic and accepts raw JSON as parameter during the
+POST call with any method:
+
+```sql
+SELECT * FROM api.get_ledger_account_json('username'); -- default
+SELECT * FROM api.get_ledger_account_json('username', NULL); -- all
+```
+
+Copyright © [2025] Debmalya Pramanik (ZenithClow)
+********************************************************************/
+
+CREATE OR REPLACE FUNCTION api.get_ledger_account_json (
+  p_username VARCHAR(16)
+  -- ! use like clause to search, with equal functionality
+  -- by either passing % wild character search, or values directly
+  -- returns all debit account types by default for querying
+  , p_account_type_key VARCHAR(7) = 'DBT/%'
+)
+RETURNS JSON AS $$
+BEGIN
+  RETURN (
+    SELECT
+      JSON_AGG(JSON_BUILD_OBJECT(
+        t.ledger_account_id
+        , t.account_name
+      ))
+    FROM (
+      WITH user_role_cte AS (
+        SELECT user_role FROM public.user_account_detail
+        WHERE username = p_username
+      ), usernames_cte AS (
+        SELECT DISTINCT username
+        FROM public.user_account_detail
+        WHERE
+          CASE
+            WHEN (SELECT user_role FROM user_role_cte) = 'ROOT' THEN TRUE
+          ELSE username = p_username OR managed_by = p_username
+          END
+      )
+
+      SELECT
+        ledger_account_id
+        , account_name
+      FROM public.ledger_account_detail
+      WHERE
+        account_owner IN (SELECT username FROM usernames_cte)
+        AND CASE
+          WHEN p_account_type_key IS NULL THEN TRUE
+          ELSE account_type_key LIKE p_account_type_key
+          END
+      ORDER BY account_opened_on
+    ) t
+  );
+END
+$$ LANGUAGE plpgsql;
