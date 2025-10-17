81385098f2198f3d3473496457ae044f8e0d1e1c
ZenithClown <debmalyapramanik.005@gmail.com>
2025-10-17 18:12:14 +0530

    📝 added docs for meta.root_user_exists()

--- database/schema/meta/functions/root_user_exists.sql
+++ database/schema/meta/functions/root_user_exists.sql
@@ -1,3 +1,22 @@
+/********************************************************************
+Check if ROOT Users Exists, Fail "On Exists" Allow Otherwise
+
+A database can have only one super administrator (i.e., a root user,
+like in *nix based system) and the function is created to raise an
+exception if a new user with root role is being assigned. By database
+design, a root user is the owner of the database while a sudo user is
+like a family head or a manager who can oversee and check the total
+expense and other details for an user.
+
+The trigger function is enabled on ``public.user_account_detail``
+with a clause on both insert and update as - (I) we do not allow to
+create more than one root user, and (II) we do not accidentaly want to
+assign ROOT role to a database where a super administrator already
+exists - check trigger on table for more details
+
+Copyright © [2025] Debmalya Pramanik (ZenithClown)
+********************************************************************/
+
 CREATE FUNCTION meta.root_user_exists() RETURNS TRIGGER
 LANGUAGE plpgsql AS
 $$
